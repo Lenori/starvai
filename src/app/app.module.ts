6e32ca360d026d938d1bd1f42b2448a9aba688bc
Leo Lenori <leolenori@gmail.com>
2020-03-17 10:34:00 -0300

    backup 2020-03-17 10:33

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { ProjetosComponent } from './views/projetos/projetos.component';
+import { TitleComponent } from './includes/title/title.component';
+import { HeaderComponent } from './includes/header/header.component';
+import { FooterComponent } from './includes/footer/footer.component';
+import { EtapasComponent } from './includes/etapas/etapas.component';
+import { OpcoesComponent } from './includes/opcoes/opcoes.component';
+
+const appRoutes = [
+
+  { path: 'projetos', component: ProjetosComponent}
+
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjetosComponent,
+    TitleComponent,
+    HeaderComponent,
+    FooterComponent,
+    EtapasComponent,
+    OpcoesComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: false} // <-- debugging purposes only
+    ),
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
